4e4c75d2f202b1f1caae45309fa9f083f8a81914
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-05-09 17:47:30 +0200

    first commit

---
+++ database/migrations/2022_05_09_152457_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->mediumText('title');
+            $table->text('description')->nullable(true);
+            $table->string('thumb', 100);
+            $table->string('price', 20);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
