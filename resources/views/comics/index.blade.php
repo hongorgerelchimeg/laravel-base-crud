e6c343c4b13ad898d8cb1f8a18852a73ca9433fd
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-05-13 16:29:06 +0200

    front-end development

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,13 +3,55 @@
 @section('title', 'Comics listing | DC Comics')
 
 @section('content')
-
-    <ul>
+    <div class="jumbotron">
+        <img src=" {{ asset('images/jumbotron.jpg') }}" alt="">
+    </div>
+    <div class="current-series">
         @foreach ($comics as $comic)
-        <li>
-            <a href="{{ route('comics.show', $comic->id) }}">{{ $comic['title'] }} </a>
-        </li>
-        @endforeach
-    </ul>
+            <a href="{{ route('comics.show', $comic->id) }}">
+                <div class="card">
 
+                        {{-- <?php dd($comic) ?> --}}
+                        <div class="img-holder">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+                        </div>
+
+                        <h3>{{ $comic['series'] }}</h3>
+                    </a>
+                </div>
+            </a>
+        @endforeach
+    </div>
+
+    <div class="load-more-btn">
+        <button>Load More</button>
+    </div>
+
+    <div class="container-sec2">
+        <div class="section2">
+            <div class="text-image-card">
+                <img src="{{asset('images/buy-comics-digital-comics.png')}}" alt="">
+                <span>digfital comics</span>
+            </div>
+            <div class="text-image-card">
+                <img src="{{asset('images/buy-comics-merchandise.png')}}" alt="">
+                <span>digfital comics</span>
+            </div>
+            <div class="text-image-card">
+                <img src="{{asset('images/buy-comics-subscriptions.png')}}" alt="">
+                <span>digfital comics</span>
+            </div>
+            <div class="text-image-card">
+                <img src="{{asset('images/buy-comics-shop-locator.png')}}" alt="">
+                <span>digfital comics</span>
+            </div>
+            <div class="text-image-card">
+                <img src="{{asset('images/buy-dc-power-visa.svg')}}" alt="">
+                <span>digital comics</span>
+            </div>
+        </div>
+    </div>
 @endsection
+
+
+
